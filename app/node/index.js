1b89fe0a7c542c6fbefab487d116f782f40bd94e
YummYume <givsoul@gmail.com>
2022-11-29 10:19:42 +0100

    Added env variables for ports and removed root user

--- app/node/index.js
+++ app/node/index.js
@@ -1,12 +1,11 @@
 import express from 'express';
 
 const app = express();
+const port = process.env.NODE_PORT ?? 5000;
 
 app.get('/', (req, res) => {
     res.send('Hey, I\'m an API');
 });
-
-const port = 5000;
 
 app.listen(port, () => {
     console.log(`server running on port ${port}`);
